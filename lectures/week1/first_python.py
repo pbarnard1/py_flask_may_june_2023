b7ae0408135100eb4bd91e15492d541c6e6ea0d9
unknown <pabarnard@outlook.com>
2023-05-02 18:59:25 -0700

    Added Tuesday week 1 lecture code

---
+++ lectures/week1/first_python.py
@@ -0,0 +1,15 @@
+first_variable = 10 # Variable and function names should be snake_case in Python
+
+# boolean variables
+is_happy = True # Capitalize your boolean values!!  True/False, NOT true/false (note the capitalization)
+
+# Placeholder value (None is the closest equivalent)
+mystery_variable = None
+
+# Printing values and using strings
+x = 8 # Change this value so that you can see the strings change
+
+print(f"The value of x is {x}") # f-string
+print("The value of x is",x) # One way to combine - only works for print statements
+print("The value of x is ",x) # This leads to two spaces between instead of 1, as the comma will add the space for you
+print("The value of x is " + str(x)) # MUST use the str() function to convert a number to a string; this also works when not using a print statement
---
+++ lectures/week1/js_comparison.js
@@ -0,0 +1,9 @@
+let myVariable = 10; // camelCase
+
+let isHappy = true;
+
+// if (myVariable == 10) {
+
+// }
+
+let someVariable = null;
