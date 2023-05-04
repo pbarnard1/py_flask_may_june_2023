dae86cede07b44c6aefc276ff91d4ec68fb4dd93
unknown <pabarnard@outlook.com>
2023-05-04 16:00:03 -0700

    Added Thursday week 1 office hour code and lecture outline

---
+++ lectures/week1/Week1ThursdayOutline.md
@@ -0,0 +1,36 @@
+# Python PT - outline for Week 1: Thursday, May 4, 2023
+
+## Announcements:
+- Please install MySQL Workbench and Server this weekend!
+- Don't forget about the class project poll on Discord!
+
+## Reminders:
+- Please remember to get an early start with assignments and discussion topics every week!
+- Don't forget about the 20-minute rule and Dojo Hall!
+- Remember to read the material BEFORE the lecture that day!
+- To pass the course:
+    - Need at least 15 out of 16 core assignments (try to do as many practice assignments as you can)
+    - Need at least 11 (preferably 12) out of 16 discussion topics
+    - Pass the belt exam (have 3 attempts, need to get 15+ core assignments done)
+
+## Core assignments this week:
+- For Loop Basic I
+- Nested Dictionaries and Lists
+
+Need at least 15 out of 16 done total this course to satisfy this requirement and to be eligible for the exam at the end of week 6!
+
+## Discussion topics reminder:
+- Due Sunday by 11:59 PM Pacific!
+
+Need at least 11 (recommended 12) out of 16 discussion topics to satisfy this requirement!
+
+## Lecture outline:
+- Functions
+    - Print vs. return
+- Dictionaries
+    - Grabbing and changing values
+    - Looping through them
+- Combined data types (we'll see how far we get today) - such as lists of dictionaries
+
+- After main lecture:
+    - Look at problem from Functions Basic II practice assignment that combines many of the concepts from this week
---
+++ office_hours/week1/group_challenges_week_1.py
@@ -0,0 +1,53 @@
+"""
+Challenge 1: What's your name? (From HackerRank - https://www.hackerrank.com/challenges/whats-your-name/problem?isFullScreen=true)
+
+You'll have two variables for first name and last name.  Your goal is to print a string in the following format:
+'Hello first_name last_name!  You just delved into Python!'
+
+For example, if the first name is "John" and the last name is "Doe", then you should print:
+'Hello John Doe!  You just delved into Python!'
+"""
+
+
+"""
+Challenge 2:
+Create a loop that will print *every other* value in a list, starting with the value at index 0, index 2, etc.
+
+For example (feel free to make up your own lists - use your own variables and values!):
+my_list = [8, 1, "Hello", "green", True, 4, -1.5] # You should print 8, "Hello", True and -1.5 in this example
+# Your loop goes here.
+"""
+
+
+"""
+Challenge 3:
+Write a function that accepts a string as input and returns the number of vowels found in the string.
+For this exercise, "a", "e", "i", "o" and "u" are vowels; we will not count "y" for this challenge.
+If it helps, assume the string is all lower-case for now.  Then try to make it work regardless of if the
+letters are upper- or lower-case.
+
+For example: given "tommy", return 1 as "o" is a vowel, but "y" is not.  "adrian" should return 3.
+"""
+
+
+"""
+Challenge 4:
+Given a list of dictionaries as input in the format below, return a new list containing only the names.
+
+Format:
+some_variable = [
+    {
+        "name": "Adrian",
+        "favorite_food": "Pizza",
+        "favorite_number": 88
+    },
+    {
+        "name": "Jenny",
+        "favorite_food": "Sushi",
+        "favorite_number": 24
+    },
+]
+
+In this example, we should get ["Adrian", "Jenny"] as those are the names found.  Make this work for all lists,
+so please make up your own examples!
+"""
